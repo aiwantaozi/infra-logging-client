2eec6b726bfab3c31b66d28a4bc6c59fc9b951fd
Aiwantaozi <michelia.feng@gmail.com>
2017-11-17 16:10:18 +0800

    add logging auth

--- logging/v1/types.go
+++ logging/v1/types.go
@@ -6,15 +6,20 @@ import (
 )
 
 const (
+	ClusterNamespace = "cattle-system"
+	GroupName        = "rancher.com"
+	Version          = "v1"
+
 	LoggingsKind          = "Logging"
 	LoggingResourcePlural = "loggings"
 	LoggingName           = "rancherlogging"
-	GroupName             = "rancher.com"
-	Namespace             = "cattle-system"
-	Version               = "v1"
-	SecretName            = "loggingsecret"
-	SecretPath            = "/fluentd/etc/k8ssecret"
-	ProviderName          = "fluentd"
+
+	LoggingAuthsKind          = "LoggingAuth"
+	LoggingAuthResourcePlural = "loggingauths"
+	LoggingAuthsName          = "rancherloggingauth"
+
+	SecretName   = "loggingsecret"
+	ProviderName = "fluentd"
 )
 
 var (
@@ -26,26 +31,49 @@ var (
 )
 
 type Target struct {
-	TargetType               string `json:"target_type"`
-	OutputTypeName           string `json:"output_type_name"`
-	OutputHost               string `json:"output_host"`
-	OutputPort               int    `json:"output_port"`
-	OutputLogstashPrefix     string `json:"output_logstash_prefix"`
-	OutputLogstashDateformat string `json:"output_logstash_dateformat"`
-	OutputTagKey             string `json:"output_tag_key"` // (optional; default=fluentd)
-	OutputLogstashFormat     bool   `json:"output_logstash_format"`
-	OutputIncludeTagKey      bool   `json:"output_include_tag_key"`
-	OutputFlushInterval      int    `json:"output_flush_interval"`
+	TargetType string `json:"target_type"`
+	// common
+	Enable              string            `json:"enable"`
+	OutputFlushInterval int               `json:"output_flush_interval"`
+	OutputTags          map[string]string `json:"output_records"`
+	// elasticsearch
+	ESHost               string `json:"es_host"`
+	ESPort               int    `json:"es_port"`
+	ESLogstashPrefix     string `json:"es_logstash_prefix"`
+	ESLogstashDateformat string `json:"es_logstash_dateformat"`
+	ESLogstashFormat     bool   `json:"es_logstash_format"`
+	ESIncludeTagKey      bool   `json:"es_include_tag_key"`
+	//splunk
+	SplunkHost       string `json:"splunk_host"`
+	SplunkPort       int    `json:"splunk_port"`
+	SplunkProtocol   string `json:"splunk_protocol"`
+	SplunkSource     string `json:"splunk_source"`
+	SplunkSourceType string `json:"splunk_sourcetype"`
+	SplunkTimeFormat string `json:"splunk_time_format"`
 }
 
 type Logging struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata"`
 	Target            `json:"target"`
+
+	SecretVersion string `json:"secretVersion"`
 }
 
 type LoggingList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
 	Items           []Logging `json:"items"`
+}
+
+type LoggingAuth struct {
+	metav1.TypeMeta        `json:",inline"`
+	metav1.ObjectMeta      `json:"metadata"`
+	EnableNamespaceLogging bool `json:"enableNamespaceLogging"`
+}
+
+type LoggingAuthList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []LoggingAuth `json:"items"`
 }
